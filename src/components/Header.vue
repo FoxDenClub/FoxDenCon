c8f9e951a3efd2157add467da319014beee94e5f
Clicky Coyote <clicky@coyote.fm>
2026-06-20 14:55:31 -0500

    chore: add branding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,10 @@
     class="flex flex-row items-center justify-between h-32 px-2 bg-primary-900 text-primary-50"
     id="header"
   >
-    <section><h1>Name of Con</h1></section>
+    <section><h1>Fox Den Con</h1></section>
     <section class="uppercase con-info">
-      <h2>Dates</h2>
-      <h3>Venue</h3>
+      <h2>February 19-21, 2027</h2>
+      <h3>DoubleTree Denver Hotel</h3>
     </section>
   </header>
 </template>
