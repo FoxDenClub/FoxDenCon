f05eb1ae8892f23867eb33d12f55141c67528403
Clicky Coyote <clicky@coyote.fm>
2026-08-27 18:19:42 -0500

    fix: icons

--- src/components/attend/Contribute.vue
+++ src/components/attend/Contribute.vue
@@ -13,8 +13,8 @@ import Card from 'primevue/card'
         Great Whiskersnoot, we all do this to make it fun for everyone! If you’re ready to apply, here are the
         categories for which we need volunteers!
       </p>
-      <div class="grid grid-flow-row grid-cols-5 gap-4 gap-y-8 justify-around">
-        <div class="row-span-1 col-span-1"><!-- img--></div>
+      <div class="grid grid-flow-row grid-cols-5 gap-4 gap-y-8 justify-around mt-16">
+        <div class="row-span-1 col-span-1"><img src="/icon-dj.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>DJs and Musicians</h4>
           <p>
@@ -22,7 +22,7 @@ import Card from 'primevue/card'
             people at the nightly dances!
           </p>
         </div>
-        <div class="row-span-1 col-span-1"><!-- img --></div>
+        <div class="row-span-1 col-span-1"><img src="/icon-artists.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>Artists</h4>
           <p>
@@ -30,7 +30,7 @@ import Card from 'primevue/card'
             so help us make it a beautiful event while keeping everything furry-made!
           </p>
         </div>
-        <div class="row-span-1 col-span-1"><!-- img --></div>
+        <div class="row-span-1 col-span-1"><img src="/icon-panels.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>Panelists and Events</h4>
           <p>
@@ -38,7 +38,7 @@ import Card from 'primevue/card'
             fun event in mind or a panel you’d like to run, we’d love to have you!
           </p>
         </div>
-        <div class="row-span-1 col-span-1"><!-- img --></div>
+        <div class="row-span-1 col-span-1"><img src="/icon-dancers.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>Dancers</h4>
           <p>
@@ -46,12 +46,12 @@ import Card from 'primevue/card'
             Head-to-Head Dance-off!
           </p>
         </div>
-        <div class="row-span-1 col-span-1"><!-- img --></div>
+        <div class="row-span-1 col-span-1"><img src="/icon-dealers.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>Dealers</h4>
           <p>Looking to sell some Merch or perhaps draw some art in the Artist Alley? This is the place to do it!</p>
         </div>
-        <div class="row-span-1 col-span-1"><!-- img --></div>
+        <div class="row-span-1 col-span-1"><img src="/icon-staff.png" /></div>
         <div class="row-span-1 col-span-4">
           <h4>General Staff</h4>
           <p>
